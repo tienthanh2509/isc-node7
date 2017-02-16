d6c69a43c825da3991a95c95142baf6bfe811df7
huy nguyen <huy.nguyen22994@gmail.com>
2017-02-16 21:55:38 +0700

    add route-angularjs - date: 16/2/2017

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,1 +1,21 @@
-var app = angular.module("QuanLyNhanSu", []);
+var app = angular.module("QuanLyNhanSu", ["ngRoute"]);
+
+// Route angular
+ app.config(['$routeProvider', '$locationProvider', function AppConfig($routeProvider, $locationProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl: 'views/templates/home.html',
+            //controller:'createConstructionCtrl'
+        })
+        .when('/addEmployee', {
+            templateUrl: 'views/templates/addEmployee.html',
+            //controller:'createConstructionCtrl'
+        })
+        // removed other routes ... *snip
+        .otherwise({
+            redirectTo: '/'
+        });
+        //enable html5Mode for pushstate ('#'-less URLs)
+        //$locationProvider.html5Mode(true);
+        //$locationProvider.hashPrefix('!');
+    }]);
