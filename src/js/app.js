4493bc5b44bb85fb123936ddeeaf802376711890
Phạm Tiến Thành <tienthanh.dqc@gmail.com>
2017-02-22 22:21:03 +0700

    use strict
    Signed-off-by: Phạm Tiến Thành <tienthanh.dqc@gmail.com>

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -132,97 +134,87 @@ $(document).ready(function() {
     });
 });
 // /Tooltip
-
-// Progressbar
-if ($(".progress .progress-bar")[0]) {
-    $('.progress .progress-bar').progressbar();
-}
-// /Progressbar
-
-// Switchery
-$(document).ready(function() {
-    if ($(".js-switch")[0]) {
-        var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
-        elems.forEach(function (html) {
-            var switchery = new Switchery(html, {
-                color: '#26B99A'
-            });
-        });
-    }
-});
-// /Switchery
-
-// iCheck
-$(document).ready(function() {
-    if ($("input.flat")[0]) {
-        $(document).ready(function () {
-            $('input.flat').iCheck({
-                checkboxClass: 'icheckbox_flat-green',
-                radioClass: 'iradio_flat-green'
-            });
-        });
-    }
-});
-// /iCheck
-
-// Table
-$('table input').on('ifChecked', function () {
-    checkState = '';
-    $(this).parent().parent().parent().addClass('selected');
-    countChecked();
-});
-$('table input').on('ifUnchecked', function () {
-    checkState = '';
-    $(this).parent().parent().parent().removeClass('selected');
-    countChecked();
-});
-
-var checkState = '';
-
-$('.bulk_action input').on('ifChecked', function () {
-    checkState = '';
-    $(this).parent().parent().parent().addClass('selected');
-    countChecked();
-});
-$('.bulk_action input').on('ifUnchecked', function () {
-    checkState = '';
-    $(this).parent().parent().parent().removeClass('selected');
-    countChecked();
-});
-$('.bulk_action input#check-all').on('ifChecked', function () {
-    checkState = 'all';
-    countChecked();
-});
-$('.bulk_action input#check-all').on('ifUnchecked', function () {
-    checkState = 'none';
-    countChecked();
-});
-
-function countChecked() {
-    if (checkState === 'all') {
-        $(".bulk_action input[name='table_records']").iCheck('check');
-    }
-    if (checkState === 'none') {
-        $(".bulk_action input[name='table_records']").iCheck('uncheck');
-    }
-
-    var checkCount = $(".bulk_action input[name='table_records']:checked").length;
-
-    if (checkCount) {
-        $('.column-title').hide();
-        $('.bulk-actions').show();
-        $('.action-cnt').html(checkCount + ' Records Selected');
-    } else {
-        $('.column-title').show();
-        $('.bulk-actions').hide();
-    }
-}
-
+//
+// // Progressbar
+// if ($(".progress .progress-bar")[0]) {
+//     $('.progress .progress-bar').progressbar();
+// }
+// // /Progressbar
+//
+// // Switchery
+// $(document).ready(function() {
+//     if ($(".js-switch")[0]) {
+//         var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+//         elems.forEach(function (html) {
+//             var switchery = new Switchery(html, {
+//                 color: '#26B99A'
+//             });
+//         });
+//     }
+// });
+// // /Switchery
+//
+//
+// // /iCheck
+//
+// // Table
+// $('table input').on('ifChecked', function () {
+//     checkState = '';
+//     $(this).parent().parent().parent().addClass('selected');
+//     countChecked();
+// });
+// $('table input').on('ifUnchecked', function () {
+//     checkState = '';
+//     $(this).parent().parent().parent().removeClass('selected');
+//     countChecked();
+// });
+//
+// var checkState = '';
+//
+// $('.bulk_action input').on('ifChecked', function () {
+//     checkState = '';
+//     $(this).parent().parent().parent().addClass('selected');
+//     countChecked();
+// });
+// $('.bulk_action input').on('ifUnchecked', function () {
+//     checkState = '';
+//     $(this).parent().parent().parent().removeClass('selected');
+//     countChecked();
+// });
+// $('.bulk_action input#check-all').on('ifChecked', function () {
+//     checkState = 'all';
+//     countChecked();
+// });
+// $('.bulk_action input#check-all').on('ifUnchecked', function () {
+//     checkState = 'none';
+//     countChecked();
+// });
+//
+// function countChecked() {
+//     if (checkState === 'all') {
+//         $(".bulk_action input[name='table_records']").iCheck('check');
+//     }
+//     if (checkState === 'none') {
+//         $(".bulk_action input[name='table_records']").iCheck('uncheck');
+//     }
+//
+//     var checkCount = $(".bulk_action input[name='table_records']:checked").length;
+//
+//     if (checkCount) {
+//         $('.column-title').hide();
+//         $('.bulk-actions').show();
+//         $('.action-cnt').html(checkCount + ' Records Selected');
+//     } else {
+//         $('.column-title').show();
+//         $('.bulk-actions').hide();
+//     }
+// }
+//
 // Accordion
 $(document).ready(function() {
     $(".expand").on("click", function () {
         $(this).next().slideToggle(200);
-        $expand = $(this).find(">:first-child");
+        var $expand = $(this).find(">:first-child");
 
         if ($expand.text() == "+") {
             $expand.text("-");
