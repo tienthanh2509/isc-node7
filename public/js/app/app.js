e25523d8f1c32506ad86b18ed7000f0e09e68052
Phạm Tiến Thành <tienthanh.dqc@gmail.com>
2017-02-22 13:53:54 +0700

    Update app.js

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,34 +1,40 @@
+'use strict';
+
 var app = angular.module("QuanLyNhanSu", ["ngRoute", "ngFileUpload"]);
 
 // Route angular
- app.config(['$routeProvider', '$locationProvider', function AppConfig($routeProvider, $locationProvider) {
-        $routeProvider
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider) {
+    $routeProvider
+    // Index
         .when('/', {
             templateUrl: 'views/templates/home.html',
             //controller:'createConstructionCtrl'
         })
-        .when('/addEmployee', {
-            templateUrl: 'views/templates/addEmployee.html',
+
+        // Quản lý phòng ban
+        .when('/department', {
+            templateUrl: 'views/templates/department.html',
             //controller:'createConstructionCtrl'
         })
-        .when('/updateEmployee', {
-            templateUrl: 'views/templates/updateEmployee.html',
-            //controller:'createConstructionCtrl'
-        })
+
+        // Quản lý chức vụ
         .when('/role', {
             templateUrl: 'views/templates/role.html',
             //controller:'createConstructionCtrl'
         })
 
-        .when('/department', {
-            templateUrl: 'views/templates/department.html',
+        // Quản lý nhân viên
+        .when('/employee/add', {
+            templateUrl: 'views/templates/addEmployee.html',
             //controller:'createConstructionCtrl'
         })
+        .when('/employee/update', {
+            templateUrl: 'views/templates/updateEmployee.html',
+            //controller:'createConstructionCtrl'
+        })
+
         // removed other routes ... *snip
         .otherwise({
             redirectTo: '/'
         });
-        //enable html5Mode for pushstate ('#'-less URLs)
-        //$locationProvider.html5Mode(true);
-        //$locationProvider.hashPrefix('!');
-    }]);
+}]);
