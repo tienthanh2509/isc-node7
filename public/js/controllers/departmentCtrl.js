c1f1dd8bab9e3b7b942df82d34802020be591ecc
Phạm Tiến Thành <tienthanh.dqc@gmail.com>
2017-02-24 00:28:25 +0700

    Show danh sách phòng ban
    Signed-off-by: Phạm Tiến Thành <tienthanh.dqc@gmail.com>

--- public/js/controllers/departmentCtrl.js
+++ public/js/controllers/departmentCtrl.js
@@ -2,6 +2,7 @@
 
 /**
  * Created by AnhDuc on 22/02/2017.
+ * Updated by PhamThanh on 24/02/2017.
  */
 app.controller('departmentCtrl', ['$scope', 'QLNS', function ($scope, QLNS) {
     $scope.department = {
@@ -9,6 +10,11 @@ app.controller('departmentCtrl', ['$scope', 'QLNS', function ($scope, QLNS) {
         MoTa: '',
         TinhTrang: 1
     };
+
+    QLNS.department.GET().then(function(res){
+        $scope.loading = false;
+        $scope.departments = res.data;
+    });
 
     $scope.Save = function () {
         QLNS.department.POST($scope.department).then(function (res) {
