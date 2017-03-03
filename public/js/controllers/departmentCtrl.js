5ab732bda4f85372ab281d1e7e80f5ac0c29feee
Phạm Tiến Thành <tienthanh.dqc@gmail.com>
2017-03-03 09:05:42 +0700

    Cập nhật phòng ban, chức vụ

--- public/js/controllers/departmentCtrl.js
+++ public/js/controllers/departmentCtrl.js
@@ -5,24 +5,34 @@
  * Updated by PhamThanh on 24/02/2017.
  */
 app.controller('departmentCtrl', ['$scope', 'QLNS', function ($scope, QLNS) {
-    $scope.department = {
-        MaPB: null,
-        TenPB: '',
-        MoTa: '',
-        TinhTrang: 1
+    // 1. Làm tươi nội dung
+    $scope.refresh = function () {
+        $scope.loading = null;
+        $scope.departments = null;
+
+        // Clear biến tạm
+        $scope.department = {
+            MaPB: null,
+            TenPB: '',
+            MoTa: '',
+            TinhTrang: 1
+        };
+
+        // Truy vấn API
+        QLNS.department.getDepartmentWithLeader().then(function (res) {
+            $scope.loading = false;
+            $scope.departments = res.data;
+        });
     };
 
-    QLNS.department.getDepartmentWithLeader().then(function (res) {
-        $scope.loading = false;
-        $scope.departments = res.data;
-    });
-
+    // 2. Thêm mới phòng ban
     $scope.Save = function () {
         QLNS.department.POST($scope.department).then(function (res) {
             alert(res.data.message);
         })
     };
 
+    // 3. Set biến tạm bằng chỉ số index trong mảng departments
     $scope.setData = function (i) {
         var d = $scope.departments[i];
 
@@ -34,34 +44,30 @@ app.controller('departmentCtrl', ['$scope', 'QLNS', function ($scope, QLNS) {
         };
     };
 
+    // 4. Cập nhật phòng ban
     $scope.updateDepartment = function () {
         console.log($scope.department);
 
-        $scope.saving = true;
+        // Hiện nút bấm loading
+        $scope.loading = true;
+        // Gửi dữ liệu cập nhật lên API
         QLNS.department.UPDATE($scope.department).then(function (response) {
             console.log(response);
 
-            $scope.saving = false;
-            $scope.department = {
-                MaPB: null,
-                TenPB: '',
-                MoTa: '',
-                TinhTrang: 1
-            };
+            // Tải lại nội dung trang
+            $scope.loading = false;
+            $scope.refresh();
 
-            QLNS.department.getDepartmentWithLeader().then(function (res) {
-                $scope.loading = false;
-                $scope.departments = res.data;
-            });
-
+            // Đóng modal
             $('#editModal').modal('hide');
         }, function (response) {
             // TODO: Thiết lập thông báo lỗi
             alert('Some thing went wrong!');
-            $scope.saving = false;
+            $scope.loading = false;
         });
     };
 
+    // 5. Xóa phòng ban
     $scope.delete = function (id) {
         console.log('Xóa phòng ban', id, $scope.department);
 
@@ -69,19 +75,11 @@ app.controller('departmentCtrl', ['$scope', 'QLNS', function ($scope, QLNS) {
         QLNS.department.DELETE(id).then(function (response) {
             console.log(response);
 
+            // Tải lại nội dung trang
             $scope.loading = false;
-            $scope.department = {
-                MaPB: null,
-                TenPB: '',
-                MoTa: '',
-                TinhTrang: 1
-            };
+            $scope.refresh();
 
-            QLNS.department.getDepartmentWithLeader().then(function (res) {
-                $scope.loading = false;
-                $scope.departments = res.data;
-            });
-
+            // Đóng modal
             $('#deleteModal').modal('hide');
         }, function (response) {
             // TODO: Thiết lập thông báo lỗi
@@ -89,4 +87,7 @@ app.controller('departmentCtrl', ['$scope', 'QLNS', function ($scope, QLNS) {
             $scope.loading = false;
         });
     };
+
+    // Load dữ liệu ban đầu
+    $scope.refresh();
 }]);
