1d0e9e9ea57988a7a3c0c231d27ed348535329c4
huy nguyen <huy.nguyen22994@gmail.com>
2017-03-05 19:51:23 +0700

    sua phuong thuc login

--- app/models/administrator.js
+++ app/models/administrator.js
@@ -1,12 +1,21 @@
 var connection = require('../models/connection');
 
-var loginAdmin = function(admin, res){
+var loginAdmin = function(admin,req, res){
     var query = "SELECT Count(ID) AS TONTAI, TAIKHOAN, HO, TEN, HINHANH FROM ADMINISTRATOR WHERE ? AND ?";
     connection.query(query,[{TAIKHOAN: admin.TAIKHOAN}, {MATKHAU: admin.MATKHAU}], function(err, result){
         if(err){
             res.end(err.message);
         } else {
-            res.end(JSON.stringify(result[0]));
+            var tontai = JSON.stringify(result[0].TONTAI);
+            if(tontai === '0'){
+                res.json({status: '0', comment: 'Mật khẩu hoặc tài khoản không đúng!'});
+            } else if(tontai === '1') {
+                req.session.user = "admin";
+                req.session.admin = true;
+                req.session.profile = result[0];         
+                res.json({status: '1', admin: result[0]});
+                //res.render('index.ejs', {admin: JSON.stringify(result[0])})
+            }
         }
     });
 };
